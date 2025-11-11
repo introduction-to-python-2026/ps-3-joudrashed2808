c67a99119f68150adbb2e96ec3c834592996f34f
Joud Rashed <joudrashed2808@gmail.com>
2025-11-11 21:05:36 +0200

    Refactor direction handling in leibniz_series_exercise

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -3,12 +3,14 @@ def move(my_list, direction):
 
     index_of_one = new_list.index(1)
 
-    if direction == 'right' and index_of_one < len(new_list) - 1:
-        new_list[index_of_one] = 0
-        new_list[index_of_one + 1] = 1
-    elif direction == 'left' and index_of_one > 0:
-        new_list[index_of_one] = 0
-        new_list[index_of_one - 1] = 1
+    if direction == 'right':
+        if index_of_one < len(new_list) - 1:
+            new_list[index_of_one] = 0
+            new_list[index_of_one + 1] = 1
+    elif direction == 'left':
+        if index_of_one > 0:
+            new_list[index_of_one] = 0
+            new_list[index_of_one - 1] = 1
 
     return new_list
 
