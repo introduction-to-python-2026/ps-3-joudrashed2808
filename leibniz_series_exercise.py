14ec5bb4a466e14148d43d9d6277805d2f20c4a1
Joud Rashed <joudrashed2808@gmail.com>
2025-11-11 21:11:24 +0200

    Refactor index checks for boundary conditions

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -4,11 +4,12 @@ def move(my_list, direction):
     index_of_one = new_list.index(1)
 
     if direction == 'right':
-        if index_of_one < len(new_list) - 1:
+        if index_of_one != len(new_list) - 1:
             new_list[index_of_one] = 0
             new_list[index_of_one + 1] = 1
+            
     elif direction == 'left':
-        if index_of_one > 0:
+        if index_of_one != 0:
             new_list[index_of_one] = 0
             new_list[index_of_one - 1] = 1
 
